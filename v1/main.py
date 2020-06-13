4fb804a906887316f1503db22e5116f89b4c3ac3
AustinoTheGreat <53872659+AustinoTheGreat@users.noreply.github.com>
2020-06-13 00:37:56 -0400

    Update main.py

--- v1/main.py
+++ v1/main.py
@@ -8,8 +8,9 @@ import time
 import board
 import busio
 import adafruit_msa301
+import paho.mqtt.publish as publish
 
-
+# Don't need the class, realized after I made it
 class Pi:
     def __init__(self, number, mA, mB, IMU):
         self.number = number
@@ -19,11 +20,35 @@ class Pi:
         
 mSensor1 = InputDevice(17)
 mSensor2 = InputDevice(18)
+
 i2c = busio.I2C(board.SCL, board.SDA)
 msa = adafruit_msa301.MSA301(i2c)
 
+p1 = Pi(1, False, False, 1)
+
+def mqttInfo():
+    msgs = [{'topic':"RPi/1", 'payload':p1.mA}, ("RPi/1", p1.mB, 0, False), ("RPi/1", p1.IMU, 0, False)]
+    publish.multiple(msgs, hostname="test.mosquitto.org")
+
+
 
 while True:
-    print("%f %f %f" % msa.acceleration)
-    time.sleep(1)
+    #print("%f %f %f" % msa.acceleration)
+    #print(msa.acceleration, mSensor1)
+    if msa.acceleration[1] < -8:
+        p1.IMU = 1
+    elif msa.acceleration[1] > 8:
+        p1.IMU = 2
+    elif msa.acceleration[0] < -8:
+        p1.IMU = 3
+    elif msa.acceleration[0] > 8:
+        p1.IMU = 4
+    p1.mA = mSensor1.is_active
+    p1.mB = mSensor2.is_active
+    
+    print(p1.mA, p1.mB, p1.IMU)
+    mqttInfo()
+    
+    time.sleep(10)
+    
 print("Done")
