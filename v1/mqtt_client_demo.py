2a76e685fb2a43a0555d3bb08de16ee7d736070a
AustinoTheGreat <53872659+AustinoTheGreat@users.noreply.github.com>
2020-06-13 22:55:29 -0400

    Update mqtt_client_demo.py

--- v1/mqtt_client_demo.py
+++ v1/mqtt_client_demo.py
@@ -6,6 +6,7 @@
 import paho.mqtt.client as mqtt
 from PIL import Image
 import crop
+import rotate
 
 #Use this class to store multiple P5
 class Pi:
@@ -16,9 +17,8 @@ class Pi:
         self.IMU = IMU
  
 p1 = Pi(1, False, False, 1)
-img = Image.open('old_campus.jpg')
-global count
-count = 0
+path = "old_campus.jpg"
+img = Image.open(path)
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -34,24 +34,22 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
 
     msg = msg.payload.decode('UTF-8')
-    #print(msg)
-    #print(dMsg)
-    # p1.mB = dMsg[1]
-    # p1.IMU = dMsg[2]
-    # crop.main(p1.mA, p1.mB)
-    
-    if (count % 3) == 0:
-        if msg == "True":
-            p1.mA = True
-    elif (count % 3) == 1:
-        if msg == "True":
-            p1.mB = True
-    elif (count % 3) == 2:
-        p1.IMU = int(msg)
-    count = count + 1
-    print(p1.mA)
-    print(p1.mB)
-    print(p1.IMU)
+    print(msg)
+    p1.mA = msg[0]
+    p1.mB = msg[1]
+    p1.IMU = msg[2]
+    if (p1.mA == "T"):
+        p1.mA = 1
+    else:
+        p1.mA = 0
+    if (p1.mB == "T"):
+        p1.mB = 1
+    else:
+        p1.mB = 0
+    crop.main(img, p1.mA, p1.mB, p1.IMU)
+
+    #rotate.main(img, p1.IMU)
+
 
 
 
