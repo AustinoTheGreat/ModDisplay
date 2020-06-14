2b6eccddc1d8bba31e5ab9185a274c8f287c45f0
AustinoTheGreat <53872659+AustinoTheGreat@users.noreply.github.com>
2020-06-13 23:42:32 -0400

    Update main.py

--- v1/main.py
+++ v1/main.py
@@ -27,28 +27,30 @@ msa = adafruit_msa301.MSA301(i2c)
 p1 = Pi(1, False, False, 1)
 
 def mqttInfo():
-    msgs = [{'topic':"RPi/1", 'payload':p1.mA}, ("RPi/1", p1.mB, 0, False), ("RPi/1", p1.IMU, 0, False)]
-    publish.multiple(msgs, hostname="test.mosquitto.org")
+    #msgs = [{'topic':"RPi/1", 'payload':p1.mA}, ("RPi/1", p1.mB, 0, False), ("RPi/1", p1.IMU, 0, False)]
+    #publish.multiple(msgs, hostname="test.mosquitto.org")
+    publish.single("RPi/1", p1.mA[0] + p1.mB[0] + p1.IMU, hostname = "test.mosquitto.org")
 
 
 
 while True:
     #print("%f %f %f" % msa.acceleration)
     #print(msa.acceleration, mSensor1)
-    if msa.acceleration[1] < -8:
+    if msa.acceleration[1] < -7:
         p1.IMU = 1
-    elif msa.acceleration[1] > 8:
+    elif msa.acceleration[1] > 7:
         p1.IMU = 2
-    elif msa.acceleration[0] < -8:
+    elif msa.acceleration[0] < -7:
         p1.IMU = 3
-    elif msa.acceleration[0] > 8:
+    elif msa.acceleration[0] > 7:
         p1.IMU = 4
-    p1.mA = mSensor1.is_active
-    p1.mB = mSensor2.is_active
+    p1.IMU = str(p1.IMU)    
+    p1.mA = str(mSensor1.is_active)
+    p1.mB = str(mSensor2.is_active)
     
-    print(p1.mA, p1.mB, p1.IMU)
+    print(p1.mA[0] + p1.mB[0] + p1.IMU)
     mqttInfo()
     
-    time.sleep(10)
+    time.sleep(5)
     
 print("Done")
