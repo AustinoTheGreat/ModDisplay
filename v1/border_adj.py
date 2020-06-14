b192b3dd5106982aa13e14c25ec7369d6244a735
Joel Binu <joelbinu12@gmail.com>
2020-06-14 20:17:10 +0530

    Update border_adj.py
    Updated border adjustment file. It outputs two files given an image : the overall image on all the displays as well as the image for the one display we are looking to control for the presentation on Wednesday. Don't forget to change the directories for the three images if you are using this code. 
    im - the original image downloaded from the internet
    im2_new (inside border_man) - the overall image of all the displays given the magnetic sensor readings
    im2_new (main) - the single display we are controlling as of rn. 
    
    Rotation yet to be done

--- v1/border_adj.py
+++ v1/border_adj.py
@@ -24,16 +24,17 @@ def border_man(im, width, height, w_width, w_height):
     
     im2_new = add_margin(im, int((w_height - height)/2), int((w_width - width)/2),  int((w_height - height)/2), int((w_width - width)/2), (0, 0, 0))
     
-    im2_new.save('/Users/joelbinu/Desktop/ModDisplay /Image repo/image_produce.jpg', quality=95)
+    im2_new.save('/Users/joelbinu/Desktop/ModDisplay /Image repo/overall_image_produced.jpg', quality=95)
     
     print (im2_new.size)
+    
+    return im2_new
 
 def crop(im, n_width, n_height, width, height):
     im2 = im.crop(((width - n_width)/2, (height - n_height)/2, (n_width + (width - n_width)/2), (n_height + (height - n_height)/2)))
-    im2.show()
     return im2
 
-im = Image.open("/Users/joelbinu/Desktop/ModDisplay /Image repo/image_produce1.jpg")
+im = Image.open("/Users/joelbinu/Desktop/ModDisplay /Image repo/original-image.jpg")
 
 print (im.size)
 
@@ -41,7 +42,7 @@ width, height = im.size
 
 
 m1 = 1
-m2 = 1
+m2 = 0
 
 if (m1 == 0) and (m2 == 0):
     if (width > 1024) or (height > 600):
@@ -60,12 +61,27 @@ width, height = im.size
 
 
 if (m1 == 0) and (m2 == 0):
-    border_man(im, width, height, 1024, 600)
+    im2_new = border_man(im, width, height, 1024, 600)
 elif (m1 == 1) and (m2 == 0):
-    border_man(im, width, height, 2048, 600)
+    im2_new = border_man(im, width, height, 2048, 600)
 elif (m1 == 0) and (m2 == 1):
-    border_man(im, width, height, 1024, 1200)
+    im2_new = border_man(im, width, height, 1024, 1200)
 elif (m1 == 1) and (m2 == 1):
-    border_man(im, width, height, 2048, 1200)
+    im2_new = border_man(im, width, height, 2048, 1200)
+
+if (m1 == 0) and (m2 == 0):
+    width, height = img1.size
+    img2 = im2_new
+elif (m1 == 1) and (m2 == 0):
+    width, height = im2_new.size
+    img2 = im2_new.crop((width/2, 0, width, height))
+elif (m1 == 0) and (m2 == 1):
+    width, height = im2_new.size
+    img2 = im2_new.crop((0, height/2, width, height))
+elif (m1 == 1) and (m2 == 1):
+    width, height = im2_new.size
+    img2 = im2_new.crop((width/2, height/2, width, height))
+
+img2.save('/Users/joelbinu/Desktop/ModDisplay /Image repo/single-display-image.jpg', quality=95)
 
 
