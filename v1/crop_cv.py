1a2a55c3e859213a2e2aa8378c565594e94a30b6
AustinoTheGreat <53872659+AustinoTheGreat@users.noreply.github.com>
2020-06-22 23:33:53 -0400

    Update crop_cv.py

--- v1/crop_cv.py
+++ v1/crop_cv.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# maybe this link will help https://stackoverflow.com/questions/44720580/resize-image-canvas-to-maintain-square-aspect-ratio-in-python-opencv
 """
 Created on Mon Jun 22 13:26:26 2020
-
 @author: joelbinu
 """
-import cv2
+from cv2 import cv2
 
 global height
 global width
 
-image = cv2.imread('/Users/joelbinu/Desktop/ModDisplay /Image repo/Produced_images/overall_image_produced.jpg')
+image = cv2.imread('pic/old_campus.jpg')
 
 def crop(im, n_width, n_height, width, height):
 
-    image2 = im[ int ((width - n_width)/ 2) : int (((width - n_width)/ 2) + n_width), int ((height - n_height)/2) : int (((height - n_height)/2) + n_height)]
+    image2 = im[int ((height - n_height)/2) : int (((height - n_height)/2) + n_height),  int ((width - n_width)/ 2) : int (((width - n_width)/ 2) + n_width), ]
     return image2
 
 def border_man(im, width, height, w_width, w_height):
@@ -27,37 +27,36 @@ m2 = 1
 im = image
 
 (height, width) = image.shape[:2]
+print(str(height) + " " + str(width))
+
+# if (m1 == 0) and (m2 == 0):
+#     if (width > 800) or (height > 480):
+#         im = crop(image, 800, 480, width, height)
+# elif (m1 == 1) and (m2 == 0):
+#     if (width > 1600) or (height > 480):
+#         im = crop(image, 1600, 480, width, height)
+# elif (m1 == 0) and (m2 == 1):
+#     if (width > 800) or (height > 960):
+#         im = crop(image, 800, 960, width, height)
+# elif (m1 == 1) and (m2 == 1):
+#     if (width > 1600) or (height > 960):
+#         im = crop(image, 1600, 960, width, height)
+
+
 
 if (m1 == 0) and (m2 == 0):
-    if (width > 1024) or (height > 600):
-        im = crop(image, 1024, 600, width, height)
+    im2_new = border_man(im, width, height, 800, 480)
 elif (m1 == 1) and (m2 == 0):
-    if (width > 2048) or (height > 600):
-        im = crop(image, 2048, 600, width, height)
+    im2_new = border_man(im, width, height, 1600, 480)
 elif (m1 == 0) and (m2 == 1):
-    if (width > 1024) or (height > 1200):
-        im = crop(image, 1024, 1200, width, height)
+    im2_new = border_man(im, width, height, 800, 960)
 elif (m1 == 1) and (m2 == 1):
-    if (width > 2048) or (height > 1200):
-        im = crop(image, 2048, 1200, width, height)
-
-
-
-if (m1 == 0) and (m2 == 0):
-    im2_new = border_man(im, width, height, 1024, 600)
-elif (m1 == 1) and (m2 == 0):
-    im2_new = border_man(im, width, height, 2048, 600)
-elif (m1 == 0) and (m2 == 1):
-    im2_new = border_man(im, width, height, 1024, 1200)
-elif (m1 == 1) and (m2 == 1):
-    im2_new = border_man(im, width, height, 2048, 1200)
+    im2_new = border_man(im, width, height, 1600, 960)
 
 
 cv2.imshow("overall_image_displayed", im2_new)
 
 cv2.waitKey(0)
-
-cv2.destroyAllWindows()
 
 print (im2_new.shape[:2])
 
@@ -72,14 +71,14 @@ elif (m1 == 0) and (m2 == 1):
     img2 = im2_new[0 : width, int (height/2) : height]
 elif (m1 == 1) and (m2 == 1):
     (height, width) = im2_new.shape[:2]
-    img2 = im2_new[int (width/2): width, int(height/2) : height]
+    print(str(height) + " " + str(width))
+    img2 = im2_new[int(height/2) : int(height), int(width/2) : int(width)]
+    # img2 = im2_new[int (width/2): int(width), int(height/2) : int(height)]
 
 
 
 cv2.imshow("single_display_image_produced", img2)
 
 cv2.waitKey(0)
-
-cv2.destroyAllWindows()
-
 print (img2.shape[:2])
+cv2.destroyAllWindows()
