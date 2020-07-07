7b27a9caa3798b858e680669741a120d880403a9
Joel Binu <joelbinu12@gmail.com>
2020-07-07 22:32:43 +0530

    Update ftp-testing.py

--- v1/ftp-testing.py
+++ v1/ftp-testing.py
@@ -6,21 +6,19 @@ Created on Thu Jun 18 16:52:29 2020
 @author: joelbinu
 """
 
-def main(filename, ipaddress, username, password, filepath) : 
-  
-  import ftplib
+import ftplib
 
-  filename = 'single-display-image.jpg'
+filename = 'single-display-image.jpg'
 
-  ftp = ftplib.FTP('192.168.0.157')
+ftp = ftplib.FTP('192.168.0.157')
 
-  ftp.login("pi", "password")
+ftp.login("pi", "password")
 
-  ftp.cwd('files')
+ftp.cwd('files')
 
-  myfile = open('/Users/joelbinu/Desktop/ModDisplay /Image repo/Produced_images/single-display-image.jpg', 'rb')
+myfile = open('/Users/joelbinu/Desktop/ModDisplay /Image repo/Produced_images/single-display-image.jpg', 'rb')
 
-  ftp.storbinary('STOR ' + filename, myfile)
+ftp.storbinary('STOR ' + filename, myfile)
 
 
 
