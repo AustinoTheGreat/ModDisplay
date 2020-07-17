ed028d9f3c1e2d4d4d3c6aa356418d78340ff90d
AustinoTheGreat <53872659+AustinoTheGreat@users.noreply.github.com>
2020-07-17 15:05:19 -0400

    Update mqtt_client_main.py

--- v2/mqtt_client_main.py
+++ v2/mqtt_client_main.py
@@ -6,6 +6,10 @@ import crop_flex
 import ftptesting
 import ftplib
 import keyboard
+import sys
+import video_crop
+from termios import tcflush, TCIFLUSH
+import time, sys
 
 
 MQTT_BROKER = "test.mosquitto.org"
@@ -15,11 +19,15 @@ MQTT_TOPICS = [("RPi/0", 0), ("RPi/1", 0)]
 width = 0
 height = 0
 orientation = "n"
-userMsg = ""
+userMsg = "type=/status=/vid=/"
 
-picPath = input("Enter your picture's full directory path or enter d to use default: ")
+picPath = input("Enter your file's full directory path or enter d to use default: ")
 if (picPath == "d"):
     picPath = "pic/large.jpg"
+if (picPath[-3: ] == "jpg"):
+    userMsg = "type=pic/status=new/vid=/"
+elif (picPath[-3: ] == "mov"):
+    userMsg = "type=vid/status=new/vid=play/"
 
 #Use this class to store multiple Pi
 class Pi:
@@ -52,6 +60,31 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     publish.single("RPi/Master", "type=pic/status=load", hostname = "test.mosquitto.org")
     client.subscribe(MQTT_TOPICS)
+
+def fileTransfer(height, width, IMU, orientation, picPath):
+    global userMsg
+    if (userMsg[5] == "p"):
+        crop_flex.main(height, width, IMU, orientation, picPath)
+        for counter in range(0, len(MQTT_TOPICS)):
+            if (pi[counter].ip != "0"):
+                filename = ("display" + str(pi[counter].posX) + str(pi[counter].posY) + ".jpg")
+                print(filename)
+                print(pi[counter].ip)
+
+                ftptesting.main("image.jpg", pi[counter].ip, "pi", "password", "export/" + filename)
+    elif (userMsg[5] == "v"):
+        video_crop.main(height, width, IMU, orientation, picPath)
+        for counter in range(0, len(MQTT_TOPICS)):
+            if (pi[counter].ip != "0"):
+                filename = ("display" + str(pi[counter].posX) + str(pi[counter].posY) + ".mp4")
+                print(filename)
+                print(pi[counter].ip)
+
+                ftptesting.main("video.mp4", pi[counter].ip, "pi", "password", "export/" + filename)
+
+
+
+
 
 
 # The callback for when a PUBLISH message is received from the server.
@@ -111,16 +144,16 @@ def on_message(client, userdata, msg):
                     orientation = "h"
                 elif (pi[i].IMU == "3" or pi[i].IMU == "4"):
                     orientation = "v"
+                
+                fileTransfer(height, width, pi[i].IMU, orientation, picPath)
+                # crop_flex.main(height, width, pi[i].IMU, orientation, picPath)
+                # for counter in range(0, len(MQTT_TOPICS)):
+                #     if (pi[counter].ip != "0"):
+                #         filename = ("display" + str(pi[counter].posX) + str(pi[counter].posY) + ".jpg")
+                #         print(filename)
+                #         print(pi[counter].ip)
 
-                crop_flex.main(height, width, pi[i].IMU, orientation, picPath)
-
-                for counter in range(0, len(MQTT_TOPICS)):
-                    if (pi[counter].ip != "0"):
-                        filename = ("display" + str(pi[counter].posX) + str(pi[counter].posY) + ".jpg")
-                        print(filename)
-                        print(pi[counter].ip)
-
-                        ftptesting.main("image.jpg", pi[counter].ip, "pi", "password", "export/" + filename)
+                #         ftptesting.main("image.jpg", pi[counter].ip, "pi", "password", "export/" + filename)
 
 
             elif (numInUse != 0 and nextPos != "nn" and pi[i].inUse == False):
@@ -128,26 +161,65 @@ def on_message(client, userdata, msg):
                 numInUse = numInUse + 1
                 pi[i].posX = int(nextPos[0])
                 pi[i].posY = int(nextPos[1])
-                crop_flex.main(height, width, pi[i].IMU, orientation, picPath)
 
-                for counter in range(0, len(MQTT_TOPICS)):
-                    if (pi[counter].ip != "0"):
-                        filename = ("display" + str(pi[counter].posX) + str(pi[counter].posY) + ".jpg")
-                        print(filename)
-                        print(pi[counter].ip)
+                fileTransfer(height, width, pi[i].IMU, orientation, picPath)
+                # crop_flex.main(height, width, pi[i].IMU, orientation, picPath)
+                # for counter in range(0, len(MQTT_TOPICS)):
+                #     if (pi[counter].ip != "0"):
+                #         filename = ("display" + str(pi[counter].posX) + str(pi[counter].posY) + ".jpg")
+                #         print(filename)
+                #         print(pi[counter].ip)
 
-                        ftptesting.main("image.jpg", pi[counter].ip, "pi", "password", "export/" + filename)
+                #         ftptesting.main("image.jpg", pi[counter].ip, "pi", "password", "export/" + filename)
+    
+    print("hello")
+
+    if userMsg[16] == "n":
+        userMsg = userMsg.replace("new", "old")
+
+    tcflush(sys.stdin, TCIFLUSH)
+    if keyboard.read_key() == "n":
+        picPath = input("Please enter your new file's path: ")
+        if (picPath[-3: ] == "jpg"):
+            userMsg = "type=pic/status=new/vid=/"
+        elif (picPath[-3: ] == "mov"):
+            userMsg = "type=vid/status=new/vid=play/"
+        fileTransfer(height, width, pi[i].IMU, orientation, picPath)
+    elif keyboard.read_key() == "q":
+        sys. exit()
+    elif userMsg[5] == "v" and keyboard.read_key() == "p":
+        userMsg = "type=vid/status=old/vid=play/"
+    elif userMsg[5] == "v" and keyboard.read_key() == "f":
+        userMsg = "type=vid/status=old/vid=free/"
+
+    # try:
+    #     if keyboard.is_pressed("n"):
+    #         picPath = input("Please enter your new file's path: ")
+    #         if (picPath[-3: ] == "jpg"):
+    #             userMsg = "type=pic/status=new/vid=/"
+    #         elif (picPath[-3: ] == "mov"):
+    #             userMsg = "type=vid/status=new/vid=play/"
+    #         fileTransfer(height, width, pi[i].IMU, orientation, picPath)
+    #     elif keyboard.is_pressed("q"):
+    #         sys. exit()
+    #     elif keyboard.is_pressed("p") and userMsg[5] == "v":
+    #         userMsg = "type=vid/status=old/vid=play/"
+    #     elif keyboard.is_pressed("f") and userMsg[5] == "v":
+    #         userMsg = "type=vid/status=old/vid=free/"
+    # except:
+    #     pass
+        
 
 
-    try:
-        if keyboard.is_pressed("q"):
-            userMsg = "type=pic/status=unload"
-        else:
-            userMsg = "type=pic/status=load"
-    except:
-        print("nothing")
+    # try:
+    #     if keyboard.is_pressed("q"):
+    #         userMsg = "type=pic/status=unload"
+    #     else:
+    #         userMsg = "type=pic/status=load"
+    # except:
+    #     print("nothing")
     printPos()  
-
+    print(userMsg)
     publish.single("RPi/Master", userMsg, hostname = "test.mosquitto.org")
     # print("end")
 
