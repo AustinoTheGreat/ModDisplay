9f05e8ad573ac529d1d739ad8c0926cdac753120
Ethan Kwok <ethan.c.kwok@gmail.com>
2020-07-20 14:16:41 -0400

    Update streaming.sh

--- streaming.sh
+++ streaming.sh
@@ -1,1 +1,1 @@
-streamlink <youtube link> <resolution, (1080p, 720p, 360p, 144p)>
+streamlink -p "omxplayer --timeout 20 --crop put_crop_coordinates_here 'x1,y1,x2,y2' ex) 0,0,640,480" --player-fifo put_youtube_link_here 720p
