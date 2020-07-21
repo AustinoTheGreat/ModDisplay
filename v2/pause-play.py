682dcc75c8b4bd1575c10e4d292a9779d0991780
Joel Binu <joelbinu12@gmail.com>
2020-07-21 21:55:57 +0530

    Update pause-play.py

--- v2/pause-play.py
+++ v2/pause-play.py
@@ -6,7 +6,7 @@ from omxplayer.player import OMXPlayer as omxplayer
 
 video_path = '/home/pi/ftp/files/cropped_video10.mp4'
 
-player = vlc.MediaPlayer(video_path)
+player = omxplayer(video_path)
 
 player.play()
 
