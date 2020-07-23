5e46567d7245c7285f8f8b1f579b643e69075098
Joel Binu <joelbinu12@gmail.com>
2020-07-23 22:00:34 +0530

    Update streaming.py

--- workInProgress/streaming.py
+++ workInProgress/streaming.py
@@ -1,4 +1,5 @@
  
+ 
 from subprocess import Popen, check_call
 import os
 import time
@@ -8,33 +9,42 @@ import keyboard
 # rc = Popen("DISPLAY=:0 chromium 'file:///home/pi/ftp/files/image.jpg' --kiosk", shell = True)
 
 # rc = Popen("streamlink -p omxplayer --timeout 20 --player-fifo https://www.youtube.com/watch?v=MCkTebktHVc 720p", shell = True)
+global rc
+global current_time
 
-def stream(x1, y1, x2, y2):
+exit = False
+def stream(x1, y1, x2, y2, link):
 
 #     rc = Popen("streamlink -p 'omxplayer --timemout 20" + " --crop " + str(x1) + "," + str(y1) + ',' + str(x2) + "," + str(y2) + "' --player-fifo https://www.youtube.com/watch?v=MCkTebktHVc 720p", shell = True)
     
      
-    rc = Popen("streamlink -p 'omxplayer --timeout 20 --crop " + str(x1) + "," + str(y1) + "," + str(x2) + "," + str(y2) + "' --player-fifo https://www.youtube.com/watch?v=MCkTebktHVc 720p", shell = True)
+    rc = Popen("streamlink -p 'omxplayer --timeout 20 --crop " + str(x1) + "," + str(y1) + "," + str(x2) + "," + str(y2) + "' --player-fifo " + link + " 720p", shell = True)
     
-#     time.sleep(20)
+#     rc = subprocess.Popen("exec streamlink -p 'omxplayer --timeout 20 --crop " + str(x1) + "," + str(y1) + "," + str(x2) + "," + str(y2) + "' --player-fifo " + link + " 720p", stdout=subprocess.PIPE, shell =True)    
 
+    current_time = time.time()
+    
     print("hello")
 
-    if (keyboard.is_pressed('ctrl+space') == True):
-        
-        Popen("killall -s 9 omxplayer.bin", shell = True)
+stream(0,0,1280,480, 'https://www.youtube.com/watch?v=MCkTebktHVc')
 
-    print("hello")
+# while (keyboard.is_pressed('a') == False):
+#     x = x + 1
+while (exit == False):
+    
+    if (time.time() - current_time > 15):
         
+        exit = True
+
+Popen("killall -s 9 omxplayer.bin", shell = True)
+
+time.sleep(10)
     
 
 
-stream(0,0,1280,480)
-
-exit()
+print("hello")
 
 # dim of stream
 
 # height = 720
 # width = 1280
-
