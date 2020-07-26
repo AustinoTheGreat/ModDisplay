321fcd238b89d2df1b705a69b291f15bb5ef29c0
AustinoTheGreat <53872659+AustinoTheGreat@users.noreply.github.com>
2020-07-26 02:29:37 -0400

    Add files via upload

---
+++ workInProgress/mainClient.py
@@ -0,0 +1,272 @@
+
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+from PIL import Image
+import crop_flex
+import ftptesting
+import ftplib
+import keyboard
+import sys
+import video_crop
+from termios import tcflush, TCIFLUSH
+import time, sys
+
+
+MQTT_BROKER = "broker.hivemq.com"
+MQTT_PORT = 1883
+# MQTT_TOPICS = [("RPi/0", 0)]
+MQTT_TOPICS = [("RPi/0", 0), ("RPi/1", 0)]
+width = 0
+height = 0
+orientation = "n"
+userMsg = "type=/status=/vid=/prev=n"
+curTime = "0"
+
+
+picPath = input("Enter your file's full directory path or enter d to use default: ")
+if (picPath == "d"):
+    picPath = "vid/harkness.mov"
+if (picPath[-3: ] == "jpg"):
+    userMsg = "type=pic/status=new/vid=free/prev=n"
+elif (picPath[-3: ] == "mov" or picPath[-3: ] == "mp4" or picPath[-3: ] == "avi"):
+    userMsg = "type=vid/status=new/vid=free/prev=n"
+
+#Use this class to store multiple Pi
+class Pi:
+    def __init__(self, number, mA, mB, IMU, ip, posX, posY, inUse):
+        self.number = number
+        self.mA = mA
+        self.mB = mB
+        self.IMU = IMU
+        self.ip = ip
+        self.posX = posX
+        self.posY = posY
+        self.inUse = inUse
+pi = []
+nextPos = "nn"
+numInUse = 0
+
+for i in range(0, len(MQTT_TOPICS)):
+    print(i)
+    pi.append(Pi(i, "F", "F", "1", "0", 0, 0, False))
+
+# p1 = Pi(1, "F", "F", "1")
+# p2 = Pi(2, "F", "F", "1")
+
+def printPos():
+    for x in pi:
+        print("Pi Number: " + str(x.number) + " Pos: (X Y) " + str(x.posX) + " " + str(x.posY) + " State: " + str(x.inUse))
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    publish.single("RPi/Master", "type=pic/status=load", hostname = MQTT_BROKER)
+    client.subscribe(MQTT_TOPICS)
+
+def fileTransfer(height, width, IMU, orientation, picPath):
+    global userMsg
+    if (userMsg[5] == "p"):
+        crop_flex.main(height, width, IMU, orientation, picPath)
+        for counter in range(0, len(MQTT_TOPICS)):
+            if (pi[counter].ip != "0"):
+                filename = ("display" + str(pi[counter].posX) + str(pi[counter].posY) + ".jpg")
+                print(filename)
+                print(pi[counter].ip)
+
+                ftptesting.main("image.jpg", pi[counter].ip, "pi", "password", "export/" + filename)
+    elif (userMsg[5] == "v"):
+        video_crop.main(height, width, IMU, orientation, picPath)
+        for counter in range(0, len(MQTT_TOPICS)):
+            if (pi[counter].ip != "0"):
+                filename = ("display" + str(pi[counter].posX) + str(pi[counter].posY) + ".mp4")
+                print(filename)
+                print(pi[counter].ip)
+
+                ftptesting.main("video.mp4", pi[counter].ip, "pi", "password", "export/" + filename)
+
+
+
+
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    global pi
+    global nextPos
+    global numInUse
+    global width
+    global height
+    global orientation
+    global height
+    global width
+    global picPath
+    global userMsg
+    global curTime
+
+    msg = msg.payload.decode('UTF-8')
+    print(msg)
+
+    for i in range(0, len(MQTT_TOPICS)):
+        if(msg[0] == str(i)):
+            prevA = pi[i].mA
+            prevB = pi[i].mB
+
+            pi[i].mA = msg[1]
+            pi[i].mB = msg[2]
+            pi[i].IMU = msg[3]
+            pi[i].ip = msg.split("/")[1]
+            prevTime = curTime
+            curTime = msg.split("/")[2]
+            pauseState = msg.split("/")[3]
+            print(pi[i].ip)
+
+            if (prevA != pi[i].mA):
+                print("A change: " + str(i))
+                if (pi[i].IMU == "1" or pi[i].IMU == "2"):
+                    nextPos = str(pi[i].posX + 1) + str(pi[i].posY)
+                    if (pi[i].posX + 1 >= width):
+                        width = width + 1
+                elif (pi[i].IMU == "3" or pi[i].IMU == "4"):
+                    nextPos = str(pi[i].posX) + str(pi[i].posY + 1)
+                    if (pi[i].posY + 1 >= height):
+                        height = height + 1
+            elif (prevB != pi[i].mB):
+                print("B change: " + str(i))
+                if (pi[i].IMU == "1" or pi[i].IMU == "2"):
+                    nextPos = str(pi[i].posX) + str(pi[i].posY + 1)
+                    if (pi[i].posY + 1 >= height):
+                        height = height + 1
+                elif (pi[i].IMU == "3" or pi[i].IMU == "4"):
+                    nextPos = str(pi[i].posX + 1) + str(pi[i].posY)
+                    if (pi[i].posX + 1 >= width):
+                        width = width + 1
+
+            if (numInUse == 0 and nextPos == "nn"):
+                pi[i].inUse = True
+                numInUse = numInUse + 1
+                height = 1
+                width = 1
+                if (pi[i].IMU == "1" or pi[i].IMU == "2"):
+                    orientation = "h"
+                elif (pi[i].IMU == "3" or pi[i].IMU == "4"):
+                    orientation = "v"
+                
+                fileTransfer(height, width, pi[i].IMU, orientation, picPath)
+                # crop_flex.main(height, width, pi[i].IMU, orientation, picPath)
+                # for counter in range(0, len(MQTT_TOPICS)):
+                #     if (pi[counter].ip != "0"):
+                #         filename = ("display" + str(pi[counter].posX) + str(pi[counter].posY) + ".jpg")
+                #         print(filename)
+                #         print(pi[counter].ip)
+
+                #         ftptesting.main("image.jpg", pi[counter].ip, "pi", "password", "export/" + filename)
+
+
+            elif (numInUse != 0 and nextPos != "nn" and pi[i].inUse == False):
+                pi[i].inUse = True
+                numInUse = numInUse + 1
+                pi[i].posX = int(nextPos[0])
+                pi[i].posY = int(nextPos[1])
+                
+                userMsg = userMsg.replace("old", "add")
+                userMsg = userMsg.replace("play", "free")
+                # userMsg = userMsg.replace("old", "new")
+                # userMsg = userMsg.replace("play", "free")
+
+                fileTransfer(height, width, pi[i].IMU, orientation, picPath)
+                # crop_flex.main(height, width, pi[i].IMU, orientation, picPath)
+                # for counter in range(0, len(MQTT_TOPICS)):
+                #     if (pi[counter].ip != "0"):
+                #         filename = ("display" + str(pi[counter].posX) + str(pi[counter].posY) + ".jpg")
+                #         print(filename)
+                #         print(pi[counter].ip)
+
+                #         ftptesting.main("image.jpg", pi[counter].ip, "pi", "password", "export/" + filename)
+    
+
+    tcflush(sys.stdin, TCIFLUSH)
+    prev = userMsg[5]
+
+    nextStep = input(msg[0] + "Press c to continue, press q to quit everything, press n to load in new video or picture file: ")
+    print(nextStep)
+    if nextStep == "n":
+        picPath = input("Please enter your new file's path: ")
+        if (picPath[-3: ] == "jpg"):
+            userMsg = "type=pic/status=new/vid=free/prev=n"
+        elif (picPath[-3: ] == "mov" or picPath[-3: ] == "mp4" or picPath[-3: ] == "avi"):
+            userMsg = "type=vid/status=new/vid=free/prev=n"
+        fileTransfer(height, width, pi[i].IMU, orientation, picPath)
+
+    elif nextStep == "q":
+        userMsg = "type=qui/status=old/vid=play/prev=n"
+        if prev == "p":
+            userMsg = "type=qui/status=old/vid=play/prev=p"
+        elif prev == "v":
+            userMsg = "type=qui/status=old/vid=play/prev=v"
+        publish.single("RPi/Master", userMsg, hostname = MQTT_BROKER)
+        sys.exit()
+    elif userMsg[5] == "v" and nextStep == "p":
+        userMsg = userMsg.replace("free", "play")
+    elif userMsg[5] == "v" and nextStep == "f":
+        userMsg = "type=vid/status=old/vid=free/prev=n"
+    elif nextStep == "c":
+        pass
+    
+    print(userMsg)
+    if prev == "p":
+        userMsg = userMsg[:userMsg.find("prev=")+5] + "p" + userMsg[userMsg.find("prev=")+6:]
+    elif prev == "v":
+        userMsg = userMsg[:userMsg.find("prev=")+5] + "v" + userMsg[userMsg.find("prev=")+6:]
+    
+    print(userMsg)
+
+    if (float(prevTime) > float(curTime)):
+        userMsg = userMsg.split("/")[0] + "/" + userMsg.split("/")[1] + "/" + userMsg.split("/")[2] + "/" + userMsg.split("/")[3] + "/" + prevTime
+    else:
+        userMsg = userMsg.split("/")[0] + "/" + userMsg.split("/")[1] + "/" + userMsg.split("/")[2] + "/" + userMsg.split("/")[3] + "/" + curTime
+    #if keyboard.read_key() == "n":
+
+    # try:
+    #     if keyboard.is_pressed("n"):
+    #         picPath = input("Please enter your new file's path: ")
+    #         if (picPath[-3: ] == "jpg"):
+    #             userMsg = "type=pic/status=new/vid=/"
+    #         elif (picPath[-3: ] == "mov"):
+    #             userMsg = "type=vid/status=new/vid=play/"
+    #         fileTransfer(height, width, pi[i].IMU, orientation, picPath)
+    #     elif keyboard.is_pressed("q"):
+    #         sys. exit()
+    #     elif keyboard.is_pressed("p") and userMsg[5] == "v":
+    #         userMsg = "type=vid/status=old/vid=play/"
+    #     elif keyboard.is_pressed("f") and userMsg[5] == "v":
+    #         userMsg = "type=vid/status=old/vid=free/"
+    # except:
+    #     pass
+        
+
+
+    # try:
+    #     if keyboard.is_pressed("q"):
+    #         userMsg = "type=pic/status=unload"
+    #     else:
+    #         userMsg = "type=pic/status=load"
+    # except:
+    #     print("nothing")
+    printPos()  
+    print(userMsg)
+    publish.single("RPi/Master", userMsg, hostname = MQTT_BROKER)
+    if userMsg[16] == "n":
+        userMsg = userMsg.replace("new", "old")
+        userMsg = userMsg.replace("add", "old")
+    # print("end")
+
+
+# Create an MQTT client and attach our routines to it.
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(MQTT_BROKER, MQTT_PORT, 60)
+
+# https://github.com/eclipse/paho.mqtt.python
+client.loop_forever()
---
+++ workInProgress/mainMSA.py
@@ -0,0 +1,163 @@
+# MQTT Publish Demo
+# Publish two messages, to two different topics
+
+#import mqtt_publish_demo.py
+from gpiozero import InputDevice
+from time import time, sleep
+import time
+import board
+import busio
+import adafruit_msa301
+import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
+import subprocess
+import re
+# from mpu6050 import mpu6050
+from subprocess import Popen, check_call
+import os
+from omxplayer.player import OMXPlayer as omxplayer
+
+MQTT_BROKER = "broker.hivemq.com"
+MQTT_PORT = 1883
+MQTT_TOPIC = "RPi/Master"
+status = False
+curTime = "0"
+pauseState = "f"
+player = "" # omxplayer("/home/pi/ftp/files/video.mp4")
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    mqttInfo()
+    client.subscribe(MQTT_TOPIC)
+
+def on_message(client, userdata, msg):
+    global status
+    global curTime
+    global pauseState
+    global player
+    msg = msg.payload.decode("UTF-8")
+    print(msg)
+    if (msg[16] == "n" and status == False):
+        if (msg[5] == "p"):
+            print("please work")
+            rc = Popen("DISPLAY=:0 chromium 'file:///home/pi/ftp/files/image.jpg' --kiosk", shell = True)
+        elif (msg[5] == "v"):
+            player = omxplayer("/home/pi/ftp/files/video.mp4")
+            # rc = Popen("DISPLAY=:0 chromium 'file:///home/pi/ftp/files/video.mp4' --kiosk", shell = True)
+            player.play()
+            sleep(1)
+            player.pause()
+        status = True
+    elif (msg[16] == "n" and status == True):
+        if (msg[34] == "p"):
+            Popen("killall -KILL chromium", shell = True)
+        elif (msg[34] == "v"):
+            print("start")
+            player.quit()
+            # Popen("killall omxplayer", shell = True)
+            print("end")
+            
+        if (msg[5] == "p"):
+            rc = Popen("DISPLAY=:0 chromium 'file:///home/pi/ftp/files/image.jpg' --kiosk", shell = True)
+        elif (msg[5] == "v"):
+
+            #rc = Popen("DISPLAY=:0 chromium 'file:///home/pi/ftp/files/video.mp4' --kiosk", shell = True)
+            player = omxplayer("/home/pi/ftp/files/video.mp4")
+            player.play()
+            sleep(1)
+            player.pause()
+    
+    elif (msg[5] == "q"):
+        # Popen("killall -KILL chromium", shell = True)
+        # Popen("killall omxplayer", shell = True)
+        
+        if (msg[34] == "p"):
+            Popen("killall -KILL chromium", shell = True)
+        elif (msg[34] == "v"):
+        #    pass
+            player.quit()
+        sys.exit()
+    elif (msg[24] == "f" and msg[5] == "v" and msg[16] == "o" and status == True):
+        print("here")
+        curTime = str(player.position())
+        pauseState = "t"
+        player.pause()
+    elif (pauseState == "t" and msg[24] == "p" and msg[5] == "v" and msg[16] == "o" and status == True):
+        # curTime = msg.split("/")[4]
+        # player = player.seek(float(curTime))
+        player.play()
+        pauseStatus = "f"
+    elif (msg[16] == "a" and msg[5] == "v"):
+        print("YAY")
+        # Popen("killall omxplayer", shell = True)
+        #if (player.can_quit() == True):
+        #    player.quit()
+        player = omxplayer("/home/pi/ftp/files/video.mp4")
+        curTime = msg.split("/")[4]
+        print(curTime)
+        player.set_position(float(curTime))
+        # player.play()
+        state = True
+        pauseState = "f"
+        sleep(3)
+    
+    if (msg[5] == "v"):
+        curTime = str(player.position())
+    mqttInfo()
+  
+  
+# Don't need the class, realized after I made it
+class Pi:
+    def __init__(self, number, mA, mB, IMU):
+        self.number = number
+        self.mA = mA
+        self.mB = mB
+        self.IMU = IMU
+        
+mSensor1 = InputDevice(17)
+mSensor2 = InputDevice(18)
+
+i2c = busio.I2C(board.SCL, board.SDA)
+msa = adafruit_msa301.MSA301(i2c)
+# sensor = mpu6050(0x68)
+
+p1 = Pi(0, False, False, 1)
+
+def mqttInfo():
+    #msgs = [{'topic':"RPi/1", 'payload':p1.mA}, ("RPi/1", p1.mB, 0, False), ("RPi/1", p1.IMU, 0, False)]
+    #publish.multiple(msgs, hostname="test.mosquitto.org")
+    # msa = sensor.get_accel_data()
+    print(msa.acceleration)
+    if msa.acceleration[1] < -7:
+        p1.IMU = 2
+    elif msa.acceleration[1] > 7:
+        p1.IMU = 1
+    elif msa.acceleration[0] < -7:
+        p1.IMU = 4
+    elif msa.acceleration[0] > 7:
+        p1.IMU = 3
+    p1.IMU = str(p1.IMU)
+    p1.number = str(p1.number)
+    p1.mA = str(mSensor1.is_active)
+    p1.mB = str(mSensor2.is_active)
+    
+    print(p1.number + p1.mA[0] + p1.mB[0] + p1.IMU + "/" +
+          re.match("([^\s]+)", str(subprocess.getstatusoutput("hostname -I")[1])).group(0))
+
+    publish.single("RPi/" + p1.number, p1.number + p1.mA[0] + p1.mB[0] + p1.IMU + "/"
+                   + re.match("([^\s]+)", str(subprocess.getstatusoutput("hostname -I")[1])).group(0)
+                   + "/" + curTime + "/" + pauseState,
+                   hostname = "broker.hivemq.com")
+
+
+# while True:
+    
+    #print("%f %f %f" % msa.acceleration)
+    #print(msa.acceleration, mSensor1)
+    
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(MQTT_BROKER, MQTT_PORT, 60)
+client.loop_forever()
